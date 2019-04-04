84b81e925da6a6fd704213bdf66828b1d1e27a71
joshuabyker <47902849+joshuabyker@users.noreply.github.com>
2019-04-04 14:34:30 -0700

    initial

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,51 @@
-var YourCharacter;
-var Anakin;
-var Yoda;
-var DarthMaul;
-var CountDookie;
-var available
-var defend
+var HeroChoice;
+var AvailableEnemies;
+//variables for individual characters
+var Naruto;
+var Madara;
+var Tobi;
+var Gaara;
+
+
+Naruto = {
+	name: 'Naruto Uzumaki',
+	health: 500,
+	attack: 43,
+	
+};
+
+
+ Madara = {
+	name: 'Madara Uchiha',
+	health: 750,
+	attack: 37,
+
+};
+
+
+Tobi = {
+	name: 'Tobi',
+	health: 725,
+	attack: 35,
+	
+};
+
+
+ Gaara = {
+	name: 'Gaara',
+	health: 600,
+	attack: 7,
+	
+};
+///on click function on HeroChoice
+HeroChoice=[Naruto,Madara,Tobi,Gaara];
+AvailableEnemies=[Naruto,Madara,Tobi,Gaara];
+
+function chooseCharacter(){
+$("#character").click(function(){});
+HeroChoice.click();
+AvailableEnemies.click();
+};
+		function startGame() {
+			new Audio('assets/naruto.mp3').play()
+ 
